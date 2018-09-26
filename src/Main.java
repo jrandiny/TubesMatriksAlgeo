0b7bdece308e010430171e6d4d5c7f893caa34ec
jrandiny <jrandiny2@gmail.com>
2018-09-26 22:32:43 +0700

    Add OBE

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("TUBES ALGEO");
+        Scanner scan = new Scanner(System.in);
+
+        int opt = scan.nextInt();
+
+        Matriks M1 = new Matriks(3,3);
+        M1.baca();
+        M1.print();
+
+        System.out.println(M1.nolBanyak());
+
+        M1.print();
     }
 }
--- src/Matriks.java
+++ src/Matriks.java
@@ -1,9 +1,9 @@
-import java.io.Console;
 import java.util.Scanner;
 
 public class Matriks {
     private double[][] isi;
     private final int NMAX = 100;
+    private final int IDXUNDEF = -99;
 
     public Matriks(int row, int col){
         isi = new double[row][col];
@@ -40,5 +40,56 @@ public class Matriks {
 
     public void set(int row, int col, double isi){
         this.isi[row][col] = isi;
+    }
+
+    public void Tambah(int row,int rowLain){
+        this.Tambah(row,rowLain,1);
+    }
+
+    public void Tambah(int row, int rowLain, int kali){
+        //nambahin row 1 dgn row lainnya
+        for (int j = 0; j < isi[row].length; j++) {
+            isi[row][j] = isi[row][j] + kali*isi[rowLain][j];
+        }
+    }
+
+    public void Kali(int row, int X){
+        //kali isi dari row dgn X
+        for (int j = 0; j < isi[row].length; j++) {
+            isi[row][j] = isi[row][j] * X;
+        }
+    }
+
+    public void TukarRow(int row1, int row2){
+        //nuker isi row1 dengan isi row2
+        double[][] temp = new double[isi.length][isi[1].length];
+
+        for (int j = 0; j < isi[row1].length; j++) {
+            temp[row1][j] = isi[row1][j];
+            isi[row1][j] = isi[row2][j];
+            isi[row2][j] = temp[row1][j];
+        }
+    }
+
+    public int nolBanyak(){
+        //cari baris/row yang 0 nya paling banyak
+        int max = -999;
+        int NMax,row;
+
+        row = IDXUNDEF;
+
+        for (int i = 0; i < isi.length; i++) {
+            NMax = 0;
+            for (int j = 0; j < isi[i].length; j++) {
+                if (isi[i][j] == 0) {
+                    NMax = NMax + 1;
+                }
+            }
+            if (max < NMax) {
+                max = NMax;
+                row = i;
+            }
+        }
+        return row;
     }
 }
